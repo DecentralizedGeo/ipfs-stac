4997e4a01f2a5d49a691eff969e304b1ae5864d8
SethDocherty <sddocherty@gmail.com>
2025-04-05 10:57:08 -0400

    Refactor file handling in create_upload_data.py to use resolved paths for text and image files

--- tests/data/create_upload_data.py
+++ tests/data/create_upload_data.py
@@ -4,17 +4,18 @@ from pathlib import Path
 
 
 def create_text_file():
-    with open("hello.txt", "w") as f:
+    text_file = Path("tests", "data", "hello.txt").resolve()
+    with open(text_file.as_posix(), "w") as f:
         f.write("Hello World!")
-        subprocess.run(["ipfs", "add", "hello.txt"])
+        subprocess.run(["ipfs", "add", text_file.as_posix()])
 
 
 def create_image_file():
     # Create a 50x50 image
-    img_path = Path("tests", "data", "image.png").as_posix()
+    img_path = Path("tests", "data", "image.png").resolve()
     from PIL import Image
 
-    with open(img_path, "wb") as f:
+    with open(img_path.as_posix(), "wb") as f:
         image = Image.new("RGB", (50, 50))
         image_bytes = BytesIO()
         image.save(image_bytes, format="PNG")
